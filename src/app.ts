24862dd4e4818c1b7bc348272b9a5af5d00e71cd
Madhan <mmadhankumar519@gmail.com>
2025-06-12 20:41:09 +0530

    Add Swagger UI Docs

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,9 @@ import cors from 'cors'
 import profileRouter from './routes/profile.routes';
 import folderRouter from './routes/folder.routes';
 import notesRouter from './routes/notes.routes';
+import swaggerUi from 'swagger-ui-express';
+import fs from "fs"
+import YAML from "yaml"
 
 const app: Application = express();
 
@@ -11,10 +14,15 @@ app.use(express.json());
 app.use(cors())
 app.use(express.urlencoded({extended:true}))
 
-app.use('/auth', authRouter);
-app.use('/profile', profileRouter);
-app.use('/folder', folderRouter);
-app.use('/note', notesRouter);
+const swaggerFile:any = fs.readFileSync('./src/config/swaggerDocs.yaml', 'utf8');
+const swaggerDocument = YAML.parse(swaggerFile);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use('/api/auth', authRouter);
+app.use('/api/profile', profileRouter);
+app.use('/api/folder', folderRouter);
+app.use('/api/note', notesRouter);
 
 app.get('/', (req, res) => {
   res.send('Notes App API is running!');
